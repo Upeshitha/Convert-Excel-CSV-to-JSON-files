84a18d6f578a226e7fb1eb5f51d4b9bd681ccabe
Eranda Upeshitha <upeshitha@gmail.com>
2021-03-12 09:47:28 +0530

    all files are working correctly, but output path is working directory

--- src/main/java/csvConvert/CSVtoJSON.java
+++ src/main/java/csvConvert/CSVtoJSON.java
@@ -3,17 +3,41 @@ package csvConvert;
 import com.fasterxml.jackson.core.JsonEncoding;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonGenerator;
+import org.apache.commons.io.FilenameUtils;
 import org.simpleflatmapper.csv.CsvParser;
 import org.simpleflatmapper.lightningcsv.CloseableCsvReader;
+import searchingFile.FindFilesName;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.List;
 
 public class CSVtoJSON {
 
-    public static void main(String[] args) throws IOException {
-        File input = new File("E:\\User\\Downloads\\customers-1.csv");
+    private static String filePath;
+    private static FileWriter fileWriter;
+    private static List<String> filePaths;
+    private static int numofFiles;
+
+    public static void getCSVPath() throws IOException {
+        FindFilesName findFilesName = new FindFilesName();
+        filePaths = findFilesName.sendLocation();
+        numofFiles = filePaths.size();
+        String oldFileName;
+        String newFileName;
+
+        for (int i=0; i<numofFiles; i++){
+            filePath = filePaths.get(i);
+            oldFileName = filePath.substring(filePath.lastIndexOf("\\")+1);
+            newFileName = FilenameUtils.removeExtension(oldFileName).concat(".json");
+            CSVtoJSON.convertCSV(filePath,newFileName);
+        }
+    }
+
+    public static void convertCSV(String path, String fileName) throws IOException {
+        File input = new File(path);
         CloseableCsvReader reader = CsvParser.reader(input);
 
         JsonFactory jsonFactory = new JsonFactory();
@@ -21,7 +45,8 @@ public class CSVtoJSON {
         Iterator<String[]> iterator = reader.iterator();
         String[] headers = iterator.next();
 
-        try (JsonGenerator jsonGenerator = jsonFactory.createGenerator(new File("output.json"), JsonEncoding.UTF8)) {
+        try (JsonGenerator jsonGenerator = jsonFactory.createGenerator(new File(fileName),
+                JsonEncoding.UTF8)) {
 
             jsonGenerator.writeStartArray();
 
